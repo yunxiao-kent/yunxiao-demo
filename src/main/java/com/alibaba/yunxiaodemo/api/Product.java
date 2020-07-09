15700fec9eec6c6bd83f9981a8374a1321b8317f
kent0113 <kent0113@163.com>
2020-07-10 00:52:53 +0800

    create product api

---
+++ src/main/java/com/alibaba/yunxiaodemo/api/Product.java
@@ -0,0 +1,40 @@
+package com.alibaba.yunxiaodemo.api;
+
+public class Product {
+    public String name;
+    public String type;
+    public double price;
+    public int amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
--- src/main/java/com/alibaba/yunxiaodemo/api/RestAPI.java
+++ src/main/java/com/alibaba/yunxiaodemo/api/RestAPI.java
@@ -1,12 +1,21 @@
 package com.alibaba.yunxiaodemo.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class RestAPI {
 
     @RequestMapping(value = "/hello")
     public String sayHello(@RequestParam(name = "name") String name) {
         return "Hello, " + name;
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        ResponseEntity<Product> productResponseEntity = new ResponseEntity<Product>(product, HttpStatus.CREATED);
+
+        return productResponseEntity;
     }
 }
